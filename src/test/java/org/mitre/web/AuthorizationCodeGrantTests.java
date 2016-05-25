c74fc58422536b904137de23424bf7ef587e3df7
Ming <minghsieh@datacom.com.tw>
2016-05-25 09:44:03 +0800

    Some progress can login but cannot get authorization page

--- src/test/java/org/mitre/web/AuthorizationCodeGrantTests.java
+++ src/test/java/org/mitre/web/AuthorizationCodeGrantTests.java
@@ -51,7 +51,7 @@ public class AuthorizationCodeGrantTests {
 	}
 
 	@Test
-	public void testTokenAcquisitionWithRegisteredRedirect() throws Exception {
+	public void testLogin() throws Exception {
 
 		ResponseEntity<String> page = serverRunning.getForString("/openid-connect-server-webapp/login");
 		String cookie = page.getHeaders().getFirst("Set-Cookie");
@@ -61,26 +61,83 @@ public class AuthorizationCodeGrantTests {
 
 		MultiValueMap<String, String> form;
 		form = new LinkedMultiValueMap<String, String>();
-		form.add("username", "user");
-		form.add("password", "password");
+		form.add("j_username", "user");
+		form.add("j_password", "password");
 		if (matcher.matches()) {
 			form.add("_csrf", matcher.group(1));
+			System.out.println("_csrf " + matcher.group(1));
 		}
-		ResponseEntity<Void> response = serverRunning.postForStatus("/openid-connect-server-webapp/login", headers, 
+		ResponseEntity<Void> response = serverRunning.postForStatus("/openid-connect-server-webapp/j_spring_security_check", headers, 
 				form);
-		cookie = response.getHeaders().getFirst("Set-Cookie");
+		System.out.println("login status code " + response.getStatusCode());
+		if (response.getStatusCode().equals(HttpStatus.FOUND)) {
+			System.out.println("found location " + response.getHeaders().getLocation());
+		}
+		assertTrue(response.getStatusCode().is3xxRedirection());
+	}
+	
+	@Test
+	public void testTokenAcquisitionWithRegisteredRedirect() throws Exception {
 
-		headers = new HttpHeaders();
-		headers.set("Cookie", cookie);
+		HttpHeaders headers = new HttpHeaders();
 
 		// The registered redirect is /redirect, but /trigger is used as a test
 		// because it is different (i.e. not the current request URI).
 		String location = serverRunning.getForRedirect(
 				"/simple-web-app/openid_connect_login?identifier=http://localhost:8080/openid-connect-server-webapp/", headers);
-		location = authenticateAndApprove(location);
+		System.out.println("client to login location " + location);
+		String authorizeLocation = serverRunning.getForRedirect(location, headers);
+		System.out.println("client to login authorizeLocation " + authorizeLocation);
+		
+		ResponseEntity<String> page = serverRunning.getForString(authorizeLocation);//http://localhost:8080/openid-connect-server-webapp/login
+		String cookie = page.getHeaders().getFirst("Set-Cookie");
+		headers.set("Cookie", cookie);
+		Matcher matcher = Pattern.compile("(?s).*name=\"_csrf\".*?value=\"([^\"]+).*").matcher(page.getBody());
 
-		assertTrue("Redirect location should be to the original photo URL: "
-				+ location, location.contains("sparklr/redirect"));
+		MultiValueMap<String, String> form;
+		form = new LinkedMultiValueMap<String, String>();
+		form.add("j_username", "user");
+		form.add("j_password", "password");
+		if (matcher.matches()) {
+			form.add("_csrf", matcher.group(1));
+		}
+		form.add("submit", "Login");
+
+		 
+		ResponseEntity<String> response = serverRunning.postForString("/openid-connect-server-webapp/j_spring_security_check", headers, 
+				form);
+		System.out.println("login status code " + response.getStatusCode());
+		if (response.getStatusCode().equals(HttpStatus.FOUND)) {
+			System.out.println("found location " + response.getHeaders().getLocation());
+		}
+		assertTrue(response.getStatusCode().is3xxRedirection());//back to /openid-connect-server-webapp 
+		
+
+		page = serverRunning.getForString(location, headers);
+		if (page.getStatusCode().equals(HttpStatus.FOUND)) {
+			System.out.println("request get for location " + location);
+			System.out.println("get for location " + response.getHeaders().getLocation());
+		}
+		assertTrue(page.getStatusCode().is2xxSuccessful());
+		matcher = Pattern.compile("(?s).*name=\"_csrf\".*?value=\"([^\"]+).*").matcher(page.getBody());
+		// Should be on user approval page now
+		form = new LinkedMultiValueMap<String, String>();
+		form.add("user_oauth_approval", "true");
+		form.add("scope_openid", "openid");
+		form.add("scope_profile", "profile");
+		form.add("scope_email", "email");
+		form.add("scope_address", "address");
+		form.add("scope_phone", "phone");
+		if (matcher.matches()) {
+			form.add("_csrf", matcher.group(1));
+		}
+		page = serverRunning.postForString(location, form);
+		if (page.getStatusCode().equals(HttpStatus.FOUND)) {
+			System.out.println("found location " + page.getHeaders().getLocation());
+		}
+
+		assertTrue("Redirect location should be to the original URL: "
+				+ location, location.contains("simple-web-app"));
 		HttpStatus status = serverRunning.getStatusCode(location, headers);
 		assertEquals(HttpStatus.OK, status);
 	}
@@ -109,6 +166,7 @@ public class AuthorizationCodeGrantTests {
 		headers.set("Cookie", cookie);
 
 		URI securityLocation = response.getLocation();
+		if (securityLocation == null) return null;
 		String secureLocation = "";
 		try {
 			secureLocation = URLDecoder.decode(securityLocation.toString(), "UTF-8");
